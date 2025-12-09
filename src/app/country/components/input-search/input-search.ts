b152cb99b67bd623fe256f3bb5b2809af8fc2d4a
Diego Ortiz <dfortiz116@gmail.com>
2025-12-09 12:14:47 -0500

    Agregar busqueda por caché en country.service

--- src/app/country/components/input-search/input-search.ts
+++ src/app/country/components/input-search/input-search.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'input-search',
@@ -9,4 +9,16 @@ export class InputSearch {
   placeholder = input<string>('Buscar');
   inputBuscar = output<string>();
 
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup)=>{
+    const value = this.inputValue();
+    const timeout = setTimeout(()=>{
+      this.inputBuscar.emit(value);
+    }, 500);
+
+    onCleanup(()=>{
+      clearTimeout(timeout);
+    })
+  })
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable, catchError, throwError, delay} from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of, tap} from 'rxjs';
 import type { Country } from '../interfaces/country.inteface';
 import { CountryMapper } from '../../mappers/country.mapper';
 
@@ -14,22 +14,41 @@ export class CountryService {
 
   private http = inject(HttpClient);
 
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
+
   searchByCapital(query: string): Observable<Country[]>{
     query = query.toLocaleLowerCase();
+
+    if (this.queryCacheCapital.has(query)){
+      console.log('Llegando por capital');
+      return of(this.queryCacheCapital.get(query) ?? []);
+    }
+
+    console.log('Nueva busqueda');
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${ query }`)
       .pipe(
         map(resp => CountryMapper.mapRestCountriesToCountryArray(resp)),
+        tap( countries => this.queryCacheCapital.set(query, countries)),
         catchError(error => {
           return throwError(()=> new Error(`No se encontró una capital con "${query}"`));
         })
-      );
+    );
   }
 
   searchByCountry(query: string): Observable<Country[]>{
     query = query.toLocaleLowerCase();
+
+    if (this.queryCacheCountry.has(query)){
+      console.log('Llegando por country');
+      return of(this.queryCacheCountry.get(query) ?? []);
+    }
+
+    console.log('Nueva busqueda');
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${ query }`)
       .pipe(
         map(resp => CountryMapper.mapRestCountriesToCountryArray(resp)),
+        tap( countries => this.queryCacheCountry.set(query, countries)),
         // delay(2000),
         catchError(error => {
           return throwError(()=> new Error(`No se encontró un país con "${query}"`));
