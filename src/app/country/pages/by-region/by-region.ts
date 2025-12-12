ac91375b6e1dc7b28876a5e6929b30872297411c
Diego Ortiz <dfortiz116@gmail.com>
2025-12-11 20:54:35 -0500

    Preservar los resultados en by-region

--- src/app/country/pages/by-region/by-region.ts
+++ src/app/country/pages/by-region/by-region.ts
@@ -1,9 +1,26 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryList } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 import { Region } from '../../interfaces/region.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+
+function validateQueryParam(queryParam: string): Region{
+  queryParam = queryParam.toLowerCase();
+
+  const validRegions: Record<string, Region> ={
+    'africa': 'Africa',
+    'americas': 'Americas',
+    'asia': 'Asia',
+    'europe': 'Europe',
+    'oceania': 'Oceania',
+    'antarctic': 'Antarctic',
+  };
+
+  return validRegions[queryParam] ?? 'Americas'
+}
+
 
 @Component({
   selector: 'app-by-region',
@@ -21,14 +38,25 @@ export class ByRegionPage {
     'Antarctic',
   ];
 
-  selectedRegion = signal<Region|null>(null);
+  ActivatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  queryParam = this.ActivatedRoute.snapshot.queryParamMap.get('region') ?? '';
+
+  selectedRegion = linkedSignal<Region|null>(() => validateQueryParam(this.queryParam));
 
   private CountryService = inject(CountryService);
+
+
   RegionResource = rxResource(
     {
       params: () => ( {region: this.selectedRegion()} ),
       stream: ({params}) => {
         if(!params.region) return of([]);
+        this.router.navigate(['/country/by-region'],{
+          queryParams: {
+            region: params.region,
+          }
+        })
         return  this.CountryService.searchByRegion(params.region)
       }
 
